43a4f2d133de9209a1a1561a8139f7db64660a05
NixonMSilva <nixonmoreira@hotmail.com>
2018-05-05 17:13:44 -0300

    Spawn
    Enemy spawn fix

--- Assets/Scripts/EnemySpawn.cs
+++ Assets/Scripts/EnemySpawn.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemySpawn : MonoBehaviour {
+	
+	public Character player;
 
 	public GameObject enemyA;
 	public GameObject enemyB;
@@ -13,6 +15,8 @@ public class EnemySpawn : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		player = GameObject.FindWithTag ("Player").GetComponent<Character>();
+
 		spawned = 0;
 		totalSpawn = 10;
 		enemyA = Resources.Load ("prefabs/foe/Types/Foe", typeof (GameObject)) as GameObject;
@@ -22,11 +26,14 @@ public class EnemySpawn : MonoBehaviour {
 	}
 
 	void Spawn () {
-		if ((++spawned % 4) == 0) {
-			Instantiate (enemyB, this.transform.position, Quaternion.identity); 
+		float distance = Mathf.Abs (player.transform.position.x - this.transform.position.x);
+		if (distance > 5.2f && distance < 25.0f) {
+			if ((++spawned % 4) == 0) {
+				Instantiate (enemyB, this.transform.position, Quaternion.identity); 
 
-		} else {
-			Instantiate (enemyA, this.transform.position, Quaternion.identity); 
+			} else {
+				Instantiate (enemyA, this.transform.position, Quaternion.identity); 
+			}	
 		}
 	}
 }
